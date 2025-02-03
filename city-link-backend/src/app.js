c8e7bec92407b44e2d5d68da55ccd4b9655b26d1
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-02-03 21:08:15 +0530

    Initial commit.

---
+++ city-link-backend/src/app.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+
+export const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+// routes
+
+app.get("/health-check", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Health-Check : server is running fine.",
+  });
+});
