dd0322729c7455c6a62257d5c98b38e9ac0e407a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-02-03 21:52:19 +0530

    modified user modal

--- city-link-backend/src/models/user.model.js
+++ city-link-backend/src/models/user.model.js
@@ -1,49 +1,49 @@
-import { Schema , model } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     fullName: {
-        type: 'String',
-        required: [true, 'Name is required'],
-        minLength: [5, 'Name must be at least 5 charcter'],
-        maxLength: [50, 'Name should be less than 50 char'],
-        lowercase: true,
-        trim: true,
+      type: "String",
+      required: [true, "Name is required"],
+      minLength: [5, "Name must be at least 5 charcter"],
+      maxLength: [50, "Name should be less than 50 char"],
+      lowercase: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: [true , 'user email is required'],
-        unique: true,
-        lowercase: true,
-        unique: [true , 'already registered']
+      type: String,
+      required: [true, "user email is required"],
+      lowercase: true,
+      unique: [true, "already registered"],
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minLength: [8, 'Password must be at least 8 char'],
-        select: false
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be at least 8 char"],
+      select: false,
     },
-    role:{
-        type: 'String',
-        enum: ['USER', 'ADMIN' , 'SUPERADMIN'],
-        default: 'USER'
+    role: {
+      type: "String",
+      enum: ["USER", "ADMIN", "SUPERADMIN"],
+      default: "USER",
     },
     forgotPasswordToken: {
-        type: String,
+      type: String,
     },
-    
+
     forgotPasswordExpiryDate: {
-        type: Date
+      type: Date,
     },
     subscription: {
-        id: String,
-        status: String
-    }
-    // password: {
-    //     type: 'String'
-    // },
-},{
-    timestamps: true
-});
-const User = model('User' , userSchema);
+      id: String,
+      status: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
 
 export default User;
