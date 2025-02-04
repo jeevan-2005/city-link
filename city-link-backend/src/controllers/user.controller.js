692b46458a5a490beefbc8d613dc9af5a271db6d
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-02-04 21:15:03 +0530

    changes few fields

--- city-link-backend/src/controllers/user.controller.js
+++ city-link-backend/src/controllers/user.controller.js
@@ -1,75 +1,75 @@
 import User from "../models/user.model.js";
 import AppError from "../utils/error.utils.js";
 
-const register = async (req,res,next) => {
-    const { fullName , email , password, mobileNumber} = req.body;
+const register = async (req, res, next) => {
+  try {
+    const { fullName, email, password, mobileNumber } = req.body;
 
-    if(!fullName || !email || !password || !mobileNumber){
-        return next(new AppError('All field are required'))
+    if (!fullName || !email || !password || !mobileNumber) {
+      return next(new AppError("All field are required"));
     }
 
     const userExists = await User.findOne({ mobileNumber });
 
-    if(userExists){
-        return next(new AppError('Email already exists' ,400));
+    if (userExists) {
+      return next(new AppError("Email already exists", 400));
     }
 
     const user = await User.create({
-        fullName,
-        email,
-        password,
-        mobileNumber
+      fullName,
+      email,
+      password,
+      mobileNumber,
     });
 
-    if(!user){
-        return next(new AppError('User registration failed , please try again'))
+    if (!user) {
+      return next(new AppError("User registration failed , please try again"));
     }
 
     await user?.save();
 
     const token = await user.generateJWTToken();
 
-    res.cookie('token', token, cookieOptions)
-   
+    res.cookie("token", token, cookieOptions);
+
     res.status(201).json({
-        success: true,
-        message: 'User registered successfully',
-        user,
-    })
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
 };
 
-const login= async (req,res,next) => {
-    try{
-        const {email , password} = req.body;
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-    if(!email || !password){
-        return next(new AppError('All fields are required' , 400));
+    if (!email || !password) {
+      return next(new AppError("All fields are required", 400));
     }
 
     const user = await User.findOne({
-        email
-    }).select('+password');
+      email,
+    }).select("+password");
 
-    if(!user || !user.comparePassword(password)){
-        return next(new AppError('Email or password does not match' , 400))
+    if (!user || !user.comparePassword(password)) {
+      return next(new AppError("Email or password does not match", 400));
     }
 
-    const token  = await user.generateJWTToken();
-    user.password = undefined;
+    const token = await user.generateJWTToken();
 
-    res.cookie('token', token , cookieOptions);
+    res.cookie("token", token, cookieOptions);
 
     res.status(200).json({
-        success: true,
-        message: "User loggedin successfully",
-        user,
+      success: true,
+      message: "User loggedin successfully",
+      user,
     });
-    }catch(e){
-        return next(new AppError(e.message , 500))
-    }
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
 };
 
-export{
-    register,
-    login
-}
+export { register, login };
