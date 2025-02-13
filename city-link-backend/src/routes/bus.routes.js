700f188b3775285fc89b7c28d3b10b330a7de37c
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-02-13 22:33:24 +0530

    bus routes added

--- city-link-backend/src/routes/bus.routes.js
+++ city-link-backend/src/routes/bus.routes.js
@@ -1,65 +1,13 @@
 import express from "express";
 import AppError from "../utils/error.utils.js";
 import { Bus } from "../models/bus.model.js";
+import { addBus, searchBus } from "../controllers/bus.controller.js";
+import { authorizeRoles, isAuth } from "../middlewares/auth.middleware.js";
 
 const busRouter = express.Router();
 
-busRouter.post("/add-bus", async (req, res, next) => {
-    try {
-        const { busNo, routes, time } = req.body;
+busRouter.post("/add-bus", isAuth, authorizeRoles("SUPER-ADMIN"), addBus);
 
-        if (!busNo || !routes || !time) {
-            return next(new AppError("Please provide busNo, routes and time.", 400));
-        }
-
-        const bus = await Bus.create({
-            busNo,
-            routes,
-            time,
-        });
-
-        res.status(201).json({
-            success: true,
-            data: bus,
-        });
-    } catch (error) {
-        return next(new AppError(error.message, 400));
-    }
-});
-
-busRouter.get("/searchBuses", async (req, res, next) => {
-    try {
-        const { source, destination } = req.query;
-    
-        if (!source || !destination) {
-          return res
-            .status(400)
-            .send('Please provide both source and destination query parameters.');
-        }
-    
-        const buses = await Bus.aggregate([
-          {
-            $addFields: {
-              sourceIndex: { $indexOfArray: ['$routes', source] },
-              destinationIndex: { $indexOfArray: ['$routes', destination] },
-            },
-          },
-          {
-            $match: {
-              sourceIndex: { $gte: 0 },
-              destinationIndex: { $gte: 0 },
-              $expr: { $lt: ['$sourceIndex', '$destinationIndex'] },
-            },
-          },
-        ]);
-    
-        res.status(200).json({ 
-            success: true,
-            data: buses,
-        });
-      } catch (error) {
-        return next(new AppError(error.message, 400));
-      }
-});
+busRouter.get("/search-buses", searchBus);
 
 export default busRouter;
