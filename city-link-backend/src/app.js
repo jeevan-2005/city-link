7417853969ec2e00728d23d84f73269a313d429f
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-03-24 22:33:12 +0530

    CORS added

--- city-link-backend/src/app.js
+++ city-link-backend/src/app.js
@@ -12,10 +12,8 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin:"http://localhost:5173", 
+    credentials: true, // Important: Allow cookies to be sent
   })
 );
 app.use(cookieParser());
